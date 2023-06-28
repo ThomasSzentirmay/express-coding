c633b692efd734598c16d763624a705e88f331b1
Thomas Szentirmay <tlszent@outlook.com>
2023-06-28 14:34:03 -0400

    paths and sending files to browser

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// HOME/ROOT PAGE
+
 app.get('/', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.send('Hello there!');
+    serverResponseObject.sendFile(path.join(__dirname, './index.html'));
 });
 
-// Hello from the about route
+// ABOUT PAGE
 
 app.get('/about', (clientRequestObject, serverResponseObject) => {
     serverResponseObject.send('Hello from the about route');
+});
+
+// API DATA
+
+app.get('/api/data', (clientRequestObject, serverResponseObject) => {
+    serverResponseObject.send({
+        message: 'Wow, we made an API!',
+        date: new Date()
+    })
 });
 
 app.listen(3333, () => console.log('Server started on port 3333'));
