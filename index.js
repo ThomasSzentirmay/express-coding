617cde00d54e2961f116c51a682fcf7f8b663831
Thomas Szentirmay <tlszent@outlook.com>
2023-06-28 13:51:04 -0400

    server get beginnings

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const app = express();
 app.get('/', (clientRequestObject, serverResponseObject) => {
     serverResponseObject.send('Hello there!');
 });
+
+// Hello from the about route
+
+app.get('/about', (clientRequestObject, serverResponseObject) => {
+    serverResponseObject.send('Hello from the about route');
+});
+
+app.listen(3333, () => console.log('Server started on port 3333'));
