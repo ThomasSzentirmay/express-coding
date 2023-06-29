bc726f88f1012656d3fc01e05d36ed0ee114e56a
Thomas Szentirmay <tlszent@outlook.com>
2023-06-29 12:07:43 -0400

    params and query with API data

--- server.js
+++ server.js
@@ -3,25 +3,49 @@ const path = require('path');
 
 const app = express();
 
-// HOME/ROOT PAGE
+console.log(__dirname);
 
+const people = [
+  {
+    id: 1,
+    name: 'JD',
+    age: 43
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    age: 99
+  }
+];
+
+
+// ROOT/HOME FILE
 app.get('/', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.sendFile(path.join(__dirname, './index.html'));
-});
-
-// ABOUT PAGE
-
-app.get('/about', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.send('Hello from the about route');
+  serverResponseObject.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // API DATA
+app.get('/api/people', (clientRequestObject, serverResponseObject) => {
 
-app.get('/api/data', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.send({
-        message: 'Wow, we made an API!',
-        date: new Date()
-    })
+  if (!clientRequestObject.query.id) return serverResponseObject.send(people);
+
+  const userObj = people.find(personObj => personObj.id == clientRequestObject.query.id);
+
+  serverResponseObject.send(userObj ? userObj : {
+    message: 'User not found',
+    error: 404
+  });
 });
 
-app.listen(3333, () => console.log('Server started on port 3333'));
+// PARAM PLACEHOLDER
+app.get('/api/person/:id', (clientRequestObject, serverResponseObject) => {
+  const userObj = people.find(personObj => personObj.id == clientRequestObject.params.id);
+
+  serverResponseObject.send(userObj ? userObj : {
+    message: 'User not found',
+    error: 404
+  });
+});
+
+// START SERVER
+app.listen(3333, () => console.log('Server started on port 3333.'));
