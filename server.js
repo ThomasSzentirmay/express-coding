7446d38ac9388d2f75a5ab3a1c30465fc651fbc2
Thomas Szentirmay <tlszent@outlook.com>
2023-06-30 11:36:12 -0400

    changes

--- server.js
+++ server.js
@@ -3,57 +3,15 @@ const path = require('path');
 
 const app = express();
 
-// API DATA
-const people = [
-  {
-    id: 1,
-    name: 'JD',
-    age: 43
-  },
-  {
-    id: 2,
-    name: 'Bob',
-    age: 99
-  }
-];
-
-
-
 app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/about', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.sendFile(path.join(__dirname, 'public/about.html'));
+// LOAD ROUTES
+
+// Post route to retrieve the form data
+app.post('/notes', (clientReq, serverRes) => {
+  console.log(clientReq.body);
 });
 
-// ROOT/HOME FILE
-// app.get('/', (clientRequestObject, serverResponseObject) => {
-//   serverResponseObject.sendFile(path.join(__dirname, 'index.html'));
-// });
 
-// API DATA
-app.get('/api/people', (clientRequestObject, serverResponseObject) => {
-
-  if (!clientRequestObject.query.id) return serverResponseObject.send(people);
-
-  const userObj = people.find(personObj => personObj.id == clientRequestObject.query.id);
-
-  serverResponseObject.send(userObj ? userObj : {
-    message: 'User not found',
-    error: 404
-  });
-});
-
-// PARAM PLACEHOLDER
-
-app.get('/test/:something/val', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.send('You did it!')
-});
-
-app.get('/api/test', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.send({
-        message: 'This is a test object'
-    })
-});
-
-// START SERVER
 app.listen(3333, () => console.log('Server started on port 3333.'));
